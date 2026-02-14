74d824b95d816a992a9e51dfbca2a8ebc1086c26
RandomUserIK <kovac.ivan97@icloud.com>
2026-02-14 20:18:50 +0100

    feat(chapter-11): implement openid connect authentication

---
+++ src/main/kotlin/com/polarbookshop/edgeservice/user/UserController.kt
@@ -0,0 +1,42 @@
+package com.polarbookshop.edgeservice.user
+
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.annotation.AuthenticationPrincipal
+import org.springframework.security.core.context.ReactiveSecurityContextHolder
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.oauth2.core.oidc.user.OidcUser
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@RestController
+class UserController {
+
+	// @GetMapping("user")
+	// fun getUser(): Mono<User> =
+	// 	ReactiveSecurityContextHolder
+	// 		.getContext()
+	// 		.map(SecurityContext::getAuthentication)
+	// 		.map { it: Authentication ->
+	// 			(it.principal as OidcUser).let { oidcUser ->
+	// 				User(
+	// 					username = oidcUser.preferredUsername,
+	// 					firstName = oidcUser.givenName,
+	// 					lastName = oidcUser.familyName,
+	// 					roles = listOf("employee", "customer"),
+	// 				)
+	// 			}
+	// 		}
+
+	@GetMapping("user")
+	fun getUser(
+		@AuthenticationPrincipal oidcUser: OidcUser,
+	): Mono<User> =
+		User(
+			username = oidcUser.preferredUsername,
+			firstName = oidcUser.givenName,
+			lastName = oidcUser.familyName,
+			roles = listOf("employee", "customer"),
+		).toMono()
+}
---
+++ src/test/kotlin/com/polarbookshop/edgeservice/user/UserControllerTest.kt
@@ -0,0 +1,63 @@
+package com.polarbookshop.edgeservice.user
+
+import com.polarbookshop.edgeservice.config.SecurityConfiguration
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
+import org.springframework.context.annotation.Import
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.core.oidc.StandardClaimNames
+import org.springframework.security.test.web.reactive.server.SecurityMockServerConfigurers
+import org.springframework.test.context.bean.override.mockito.MockitoBean
+import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.test.web.reactive.server.expectBody
+
+@WebFluxTest(UserController::class)
+@Import(SecurityConfiguration::class)
+internal class UserControllerTest @Autowired constructor(
+	private val webClient: WebTestClient,
+) {
+	@MockitoBean
+	private lateinit var clientRegistrationRepository: ReactiveClientRegistrationRepository
+
+	@Test
+	fun whenNotAuthenticatedThen401() {
+		// WHEN
+		webClient
+			.get()
+			.uri("/user")
+			.exchange()
+			.expectStatus()
+			// THEN
+			.isUnauthorized
+	}
+
+	@Test
+	fun whenAuthenticatedThenReturnUser() {
+		// GIVEN
+		val expectedUser = User(
+			username = "john.doe",
+			firstName = "John",
+			lastName = "Doe",
+			roles = listOf("employee", "customer"),
+		)
+
+		// WHEN
+		webClient
+			.mutateWith(configureMockOidcLogin(expectedUser))
+			.get()
+			.uri("/user")
+			.exchange()
+			.expectStatus()
+			.is2xxSuccessful
+			.expectBody<User>()
+			.isEqualTo(expectedUser)
+	}
+
+	private fun configureMockOidcLogin(expectedUser: User): SecurityMockServerConfigurers.OidcLoginMutator =
+		SecurityMockServerConfigurers.mockOidcLogin().idToken {
+			it.claim(StandardClaimNames.PREFERRED_USERNAME, expectedUser.username)
+			it.claim(StandardClaimNames.GIVEN_NAME, expectedUser.firstName)
+			it.claim(StandardClaimNames.FAMILY_NAME, expectedUser.lastName)
+		}
+}
